344234512f3ff796774ada27f453d1ac4cb43f54
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-15 08:09:00 +0300

    - updated unit test content

---
+++ src/state/tasks-todolist-reducer.test.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+    AddTodolistAC,
+    ChangeTodolistFilterAC,
+    ChangeTodolistTitleAC,
+    RemoveTodolistAC,
+    todolistsReducer
+} from './todolists-reducer';
+import {v1} from 'uuid';
+import {FilterValuesType, TasksStateType, TodolistType} from '../App';
+import {AddTaskAC, ChangeTaskStatusAC, ChangeTaskTitleAC, RemoveTaskAC, tasksListsReducer} from "./tasks-reducer";
+
+test('id\'s should be the same in tasks action creator and todolist action creator', () => {
+
+
+
+});
