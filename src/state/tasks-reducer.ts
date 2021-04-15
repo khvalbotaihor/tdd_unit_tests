6f57e98a6b2fe6874c0145c27f2d26befce291de
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-15 08:38:51 +0300

    - added delete todolist case to tasks reducer

--- src/state/tasks-reducer.ts
+++ src/state/tasks-reducer.ts
@@ -67,7 +67,11 @@ export const tasksListsReducer = (state: TasksStateType, action: ActionsType): T
             stateCopy[action.id] = []
             return stateCopy
         }
-
+        case "REMOVE-TODOLIST": {
+            let stateCopy = {...state}
+            delete stateCopy[action.id]
+            return stateCopy
+        }
         default:
             throw new Error("I don't understand this type")
     }
