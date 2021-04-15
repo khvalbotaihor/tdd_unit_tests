b465e83fc23bd5b8a64ad23bfe555c4daa5967f6
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-15 08:24:58 +0300

    - added unit test pre-conditions

--- src/state/tasks-todolist-reducer.test.tsx
+++ src/state/tasks-todolist-reducer.test.tsx
@@ -11,11 +11,14 @@ import {FilterValuesType, TasksStateType, TodolistType} from '../App';
 import {AddTaskAC, ChangeTaskStatusAC, ChangeTaskTitleAC, RemoveTaskAC, tasksListsReducer} from "./tasks-reducer";
 
 test('id\'s should be the same in tasks action creator and todolist action creator', () => {
-const tasksStartState: TasksStateType = {}
-const todolistStartState: Array<TodolistType> = []
+    const tasksStartState: TasksStateType = {}
+    const todolistStartState: Array<TodolistType> = []
 
+    const newTitle = 'new Title'
+    const action = AddTodolistAC(newTitle);
 
-
+    const endTaskState = tasksListsReducer(tasksStartState, action)
+    const endTodolistState = todolistsReducer(todolistStartState, action)
 
 
 });
