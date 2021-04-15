ae789b5bfc8586d278935866dac1b1656c3074e6
khvalbotaihor <ihorkhvalbota@gmail.com>
2021-04-15 08:10:32 +0300

    - added startStates

--- src/state/tasks-todolist-reducer.test.tsx
+++ src/state/tasks-todolist-reducer.test.tsx
@@ -11,6 +11,10 @@ import {FilterValuesType, TasksStateType, TodolistType} from '../App';
 import {AddTaskAC, ChangeTaskStatusAC, ChangeTaskTitleAC, RemoveTaskAC, tasksListsReducer} from "./tasks-reducer";
 
 test('id\'s should be the same in tasks action creator and todolist action creator', () => {
+const tasksStartState: TasksStateType = {}
+const todolistStartState: Array<TodolistType> = []
+
+
 
 
 
